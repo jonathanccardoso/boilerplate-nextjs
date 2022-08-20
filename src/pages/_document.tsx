7aa343e3afa37e910940ef13d86d3a7c1129ce44
Jonathan Cardoso <jonathan99mour@gmail.com>
2022-08-20 14:54:19 -0300

    feat: refatorando componente document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,37 +3,17 @@ import Document, {
   Html,
   Head,
   Main,
-  NextScript
+  NextScript,
+  DocumentInitialProps
 } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
-import { NextComponentType } from 'next'
-import { PropsWithChildren } from 'react'
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: NextComponentType) => (props: PropsWithChildren) =>
-            sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
+    return initialProps
   }
 
   render() {
